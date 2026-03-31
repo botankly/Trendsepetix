9f01f5c30ccda509347f2ebfa55b94dac8034016
Ad Soyad <mail@example.com>
2026-03-31 19:51:27 +0300

    3. Hafta: Veri Madenciliği Motoru: Python ile sepet birliktelik analizini yapacak fonksiyonların (Apriori algoritması vb.) yazılması.

--- myapp/api_views.py
+++ myapp/api_views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Product, Sale
 from .serializers import ProductSerializer, SaleSerializer
+from .utils.analysis import get_associations
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -9,3 +12,9 @@ class ProductViewSet(viewsets.ModelViewSet):
 class SaleViewSet(viewsets.ModelViewSet):
     queryset = Sale.objects.all().prefetch_related('products')
     serializer_class = SaleSerializer
+
+    @action(detail=False, methods=['get'])
+    def analyze(self, request):
+        sales = self.get_queryset()
+        analysis_results = get_associations(sales)
+        return Response(analysis_results)
