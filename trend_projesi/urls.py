ffeb5c3a96afbcbbd74e30938420a7fe31ade3f2
Ad Soyad <mail@example.com>
2026-03-31 19:50:19 +0300

    Week 2: Backend Architecture & REST API Implementation

--- trend_projesi/urls.py
+++ trend_projesi/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from myapp import views  # Bizim yazdığımız kodları buraya çağırıyoruz
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp import views, api_views
+
+router = DefaultRouter()
+router.register(r'products', api_views.ProductViewSet)
+router.register(r'sales', api_views.SaleViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Yönetim paneli yolu
-    path('', views.index, name='index'),  # Ana sayfa (artık vitrin görünecek)
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('api/', include(router.urls)),
 ]
